105e209bd07445726e7e4e57080c2f0393bc4ba1
jolean lim <joleanflima@outlook.com>
2020-09-22 09:45:01 -0400

    finito

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
+  root 'welcome$index'
+
+  get 'articles#index'
+
+  get 'articles#create'
+
+  get 'articles#new'
+
+  get 'articles#edit'
+
+  get  'articles#show'
+
+  patch 'articles#update'
+
+  put 'articles#update'
+
+  Delete 'articles#destroy'
+
+  resources :articles do
+    resources :comments
+  end
   
-  articles Get /articles(.:format)
-  articles#index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
